19830c182d814806c0fa40d807c40af703d99fe2
Daniel West <dwest at tux dot appstate dot edu>
2010-07-01 07:14:34 -0400

    Two players moving around in a room!

--- server.py
+++ server.py
@@ -4,25 +4,36 @@ from threading import Thread, Semaphore
 import SocketServer
 from multiprocessing import Pipe, Manager, Queue
 from select import select
+from copy import deepcopy
+import json
 
 class ThreadedTCPRequestHandler(SocketServer.StreamRequestHandler):
 
     def handle(self):
         p = self.server.chatserver.getPipe()
+        q = self.server.gameserver.getPipe()
+
         self.data = ""
         while self.data != "bye":
             if p.poll():
                 self.wfile.write(p.recv())
 
+            if q.poll():
+                self.wfile.write(q.recv())
+
             readable, writable, error = select([self.request], [], [], 0)
             if self.request in readable:
                 self.data = self.rfile.readline().strip()
                 p.send("%s wrote: %s" % (self.client_address[0], self.data))
+                q.send(self.data)
 
 class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
 
     def setChatServer(self, chatserver):
         self.chatserver = chatserver
+
+    def setGameServer(self, gameserver):
+        self.gameserver = gameserver
 
 class ChatServer(Thread):
 
@@ -30,7 +41,7 @@ class ChatServer(Thread):
         Thread.__init__(self)
         self.clients = list()
         self.lock = Semaphore()
-        # Set this to false when you want to stop chat server
+        # Set this to false when you want to stop the chat server
         self.RUNNING = True
 
     def run(self):
@@ -63,13 +74,116 @@ class ChatServer(Thread):
             if c is client:
                 continue
             c.send(message)
-    
+
+class GameServer(Thread):
+
+    def __init__(self):
+        Thread.__init__(self)
+        self.clients = list()
+        self.map = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+                    [0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0],
+                    [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        self.locations = {}
+        self.lock = Semaphore()
+        # Set this to false when you want to stop the game server
+        self.RUNNING = True
+
+    def run(self):
+        while self.RUNNING:
+            self.lock.acquire()
+            for c in self.clients:
+                try:
+                    if c.poll():
+                        self.handleMessage(c, c.recv())
+                except EOFError:
+                    # Poll apparently thinks it's cute to say that there is data on the pipe when it is closed
+                    self.clients.remove(c)
+            
+            self.lock.release()
+
+    def getPipe(self):
+        server_side, client_side = Pipe()
+        self.addPipe(server_side) # Blocks for an indeterminate amount of time
+        return client_side
+
+    def addPipe(self, connection):
+        self.lock.acquire()
+        self.clients.append(connection)
+        self.lock.release()
+
+    def movePlayer(self, client, direction):
+        if self.clients.index(client) in self.locations:
+            curr_location = self.locations[self.clients.index(client)]
+        else:
+            curr_location = (7, 7)
+
+        if direction == "LEFT":
+            if self.map[curr_location[0]][curr_location[1]-1] == 1:
+                curr_location = (curr_location[0], curr_location[1]-1)
+        elif direction == "RIGHT":
+            if self.map[curr_location[0]][curr_location[1]+1] == 1:
+                curr_location = (curr_location[0], curr_location[1]+1)
+        elif direction == "UP":
+            if self.map[curr_location[0]-1][curr_location[1]] == 1:
+                curr_location = (curr_location[0]-1, curr_location[1])
+        elif direction == "DOWN":
+            if self.map[curr_location[0]+1][curr_location[1]] == 1:
+                curr_location = (curr_location[0]+1, curr_location[1])
+
+        self.locations[self.clients.index(client)] = curr_location
+        return
+
+    def getGameState(self):
+        output = deepcopy(self.map)
+
+        for i in range(0, len(output)):
+            for j in range(0, len(output[i])):
+                if output[i][j] == 0:
+                    output[i][j] = '#'
+                else:
+                    output[i][j] = '.'
+
+        for location in self.locations:
+            location = self.locations[location]
+            output[location[0]][location[1]] = '@'
+
+        s = ""
+        for row in output:
+            s += ''.join(row)
+            s += '\n'
+        
+        return s
+
+    def handleMessage(self, client, message):
+        # message should be a json encoded object
+        try:
+            message = json.loads(message)
+            self.movePlayer(client, message['move'])
+            client.send(self.getGameState())
+        except ValueError:
+            print "Not a game message!"
+        
 if __name__ == "__main__":
     # Create the chat server to pass messages between clients
     chatserver = ChatServer()
-    
-    # Start the chat server
     chatserver.start()
+
+    # Create the game server to store game state
+    gameserver = GameServer()
+    gameserver.start()
 
     # Port 0 means to select an arbitrary unused port
     HOST, PORT = "localhost", 0
@@ -79,6 +193,7 @@ if __name__ == "__main__":
 
     # Tell the socket server to use this chat server
     server.setChatServer(chatserver)
+    server.setGameServer(gameserver)
 
     # Start a thread with the server -- that thread will then start one
     # more thread for each request
@@ -93,4 +208,5 @@ if __name__ == "__main__":
         message = raw_input("Type bye to exit: ")
         
     chatserver.RUNNING = False
+    gameserver.RUNNING = False
     server.shutdown()
